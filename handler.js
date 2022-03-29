0803c346b668f7647c78428089e899e585f46169
Ben Combee <bcombee@roku.com>
2022-03-29 12:05:25 -0500

    Update to handle binary data

--- handler.js
+++ handler.js
@@ -18,33 +18,47 @@ module.exports.corsProxy = (event, context, callback) => {
     console.log(event);
     console.log(`Got request with params:`, params);
 
-    if (!params.url) {
+    if (!params || !params.url) {
         const errorResponse = {
             statusCode: 400,
             body: 'Unable get url from \'url\' query parameter'
         };
-
         callback(null, errorResponse);
-
         return;
     }
+
+    // only proxy .hex file requests
+    if (!params.url.endsWith(".hex") &&
+        !params.url.endsWith(".arduboy") &&
+        !params.url.endsWith(".json")) {
+        const errorResponse = {
+            statusCode: 400,
+            body: 'Unable get fetch non-Arduboy file'
+        };
+        callback(null, errorResponse);
+        return;
+    }
+
+    // remote accept-encoding from headers
+    delete headers['accept-encoding'];
+    delete headers['Accept-Encoding'];
+    delete headers['Accept-encoding'];
 
     return new Promise((resolve, reject) => {
         let originalRequestBody = event.body;
         request({
             url: params.url,
             method: event.httpMethod,
+	    encoding: null, // needed to prevent binary data from being UTF-8 encoded
+	    gzip: true, // allow compressed data to be returned from server
             timeout: 20000,
-            json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
+            json: null,
             headers,
         }, (err, originalResponse, body) => {
             if (err) {
                 console.log(`Got error`, err);
-
                 callback(err);
-
                 reject(err);
-
                 return;
             }
 
@@ -56,9 +70,11 @@ module.exports.corsProxy = (event, context, callback) => {
                 headers: {
                     "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
                     "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    "content-type": originalResponse.headers['content-type']
+                    "content-type": originalResponse.headers['content-type'],
+		    "x-content-type-options": "nosniff"
                 },
-                body: proxyBody
+                body: proxyBody.toString('base64'),
+		isBase64Encoded: true
             };
 
             callback(null, proxyResponse);
